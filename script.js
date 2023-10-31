49d09c3811f99ce02abf05f632577990d9faa34a
Nivea Hanley <niveashan@gmail.com>
2023-10-31 16:51:20 -0400

    Updated files

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling to section when a navigation link is clicked
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
